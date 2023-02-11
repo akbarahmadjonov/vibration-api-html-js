9dd1589b2770cfa7c70f9a8877970a5f241abc96
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-02-11 10:22:52 +0500

    Add vibration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-function simpleVibrate() {
+const simpleVibrate = document.querySelector(".vibration__button--simple");
+
+simpleVibrate.addEventListener("click", function () {
   navigator.vibrate(1000);
-}
+});
